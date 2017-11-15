185698758d13519d7a0a115caebc817e3115b581
mgbrando <mgbrando29@gmail.com>
2017-11-15 11:58:07 -0500

    Set up react page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
-import css from "./app.scss";
-console.log("testing hot module");
-/* const css = require("./app.scss");
+const css = require("./app.scss");
 
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));*/
-//console.log("Hello from app.js!");
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
